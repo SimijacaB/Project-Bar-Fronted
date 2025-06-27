07617f8c46195fdfc3a6958e67c96e0eaa3ab234
SimijacaB <yesikasimi@gmail.com>
2025-06-27 15:23:58 -0500

    Se modifica el diseño global

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
+  ]
+})
   .catch((err) => console.error(err));

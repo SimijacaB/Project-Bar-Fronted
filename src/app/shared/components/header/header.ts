07cc43e50e8b122345ec634462e46951dbce0e98
SimijacaB <yesikasimi@gmail.com>
2025-06-27 15:21:56 -0500

    Se agrega los componentes reutilizables a una carpeta shared

---
+++ src/app/shared/components/header/header.ts
@@ -0,0 +1,32 @@
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+  isScrolled = false;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrolled = scrollTop > 10;
+    const header = this.el.nativeElement.querySelector('.modern-navbar');
+    if (header) {
+      if (this.isScrolled) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    }
+  }
+}
